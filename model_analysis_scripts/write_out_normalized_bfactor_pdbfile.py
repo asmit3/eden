4cb14384791b23b1dc6d7523ecd051c219010209
Asmit Bhowmick <asmit3@gmail.com>
2025-05-29 10:20:13 -0700

    updating script to write out normalized bfactor pdb file for a selection in addition to the existing pdb file for the whole protein

--- model_analysis_scripts/write_out_normalized_bfactor_pdbfile.py
+++ model_analysis_scripts/write_out_normalized_bfactor_pdbfile.py
@@ -16,6 +16,20 @@ def run(arg):
   std = stats.unweighted_sample_standard_deviation()
   normalized_b = (bfactors - mean)/std 
   atoms.set_b(normalized_b)
+  #
+  # Limited selection to write out chain G/g statistics
+  sele_str = 'chain G or chain g'
+  isel=model.selection(sele_str).iselection()
+  ph_sel = model.select(isel).get_hierarchy()
+  atoms_sel = ph_sel.atoms()
+  ph_sel.write_pdb_file('%s_only_chainGg.pdb'%(arg[:-4]))
+  print ('Wrote out PDB file with normalized bfactor for selection: %s: %s_only_chainGg.pdb'%(sele_str, arg[:-4]))
+  if False:
+    bf=atoms_sel.extract_b()
+    import matplotlib.pyplot as plt
+    plt.hist(bf)
+    plt.show()
+  
   # Normalize bfactors here
   ph.write_pdb_file('%s_normalized_bfactors.pdb'%(arg[:-4]))
   print ('Wrote out PDB file with normalized bfactor: %s_normalized_bfactors.pdb'%(arg[:-4]))
